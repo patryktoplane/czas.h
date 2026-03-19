77114f8ce89abe2798ba69dba5fe8fd97f4ae870
patryktoplane <113851282+patryktoplane@users.noreply.github.com>
2026-03-19 21:35:28 +0100

    Refactor time setting and add comparison logic

--- main.cpp
+++ main.cpp
@@ -5,20 +5,43 @@ using namespace std;
 
 int main()
 {
-    Czas c1;
-    Czas c2;
-    Czas c3;
-    Czas c4;
+    Czas c1, c2, c3, c4, c5, suma;
 
-    c1.setCzas(1, 23, 34);
-    c2.setCzas(8, 59, 0);
-    c3.setCzas(12, 83, -20);
-    c4.setCzas(3, 19, 157);
+    cout << "ustawCzas" << endl;
+    c1.setCzas(1, 2, 3);
+    c2.setCzas(4, 5);
+    c3.setCzas(6);
 
     c1.printCzas();
     c2.printCzas();
     c3.printCzas();
+
+    cout << endl;
+    cout << "wartosci ujemne" << endl;
+    c4.setCzas(83, -20);
+    c5.setCzas(3661);
+
     c4.printCzas();
+    c5.printCzas();
+
+    cout << endl;
+    cout << "dodawanie" << endl;
+    Czas a, b;
+    a.setCzas(1, 25, 40);
+    b.setCzas(0, 50, 30);
+
+    a.printCzas();
+    b.printCzas();
+
+    suma = a + b;
+    suma.printCzas();
+
+    cout << endl;
+    cout << "porownanie" << endl;
+    if (a < b)
+        cout << "a < b" << endl;
+    else
+        cout << "a >= b" << endl;
 
     return 0;
 }
