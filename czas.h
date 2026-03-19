a16206fa48345fa288e36dfb2fbf3bdec45f6f9c
patryktoplane <113851282+patryktoplane@users.noreply.github.com>
2026-03-19 21:34:05 +0100

    Refactor Czas class to add new time manipulation methods

--- czas.h
+++ czas.h
@@ -3,17 +3,26 @@
 using namespace std;
 class Czas
 {
-public:
-	void setGodziny(int x);
-	void setMinuty(int x);
-	void setSekundy(int x);
-	void setCzas(int h, int m, int s);
-	int getGodziny();
-	int getMinuty();
-	int getSekundy();
-	void printCzas();
 private:
 	int godziny = 0;
 	int minuty = 0;
 	int sekundy = 0;
+
+	int TimeToSec();
+	void SecToTime(int x);
+
+public:
+	int getGodzina();
+	int getMinuta();
+	int getSekunda();
+
+	void setCzas(int h, int m, int s);
+	void setCzas(int m, int s);
+	void setCzas(int s);
+
+	void printCzas();
+
+	Czas operator+(Czas& nowy);
+
+	bool operator<(Czas& nowy);
 };
