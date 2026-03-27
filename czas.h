19eed281301eaf2c32f8272948edbe9fc061454d
patryktoplane <113851282+patryktoplane@users.noreply.github.com>
2026-03-27 11:13:33 +0100

    Enhance Czas class with constructors and operators
    Added constructors and comparison operators to Czas class.

--- czas.h
+++ czas.h
@@ -1,6 +1,8 @@
 #pragma once
 #include<iostream>
+
 using namespace std;
+
 class Czas
 {
 private:
@@ -20,9 +22,20 @@ public:
 	void setCzas(int m, int s);
 	void setCzas(int s);
 
+	Czas(int h, int m, int s);
+	Czas(int m, int s);
+	Czas(int s);
+	Czas();
+
 	void printCzas();
 
 	Czas operator+(Czas& nowy);
+	Czas& operator+=(Czas& nowy);
 
+	bool operator==(Czas& nowy);
 	bool operator<(Czas& nowy);
+	bool operator!=(Czas& nowy);
+	bool operator>(Czas& nowy);
+	bool operator<=(Czas& nowy);
+	bool operator>=(Czas& nowy);
 };
