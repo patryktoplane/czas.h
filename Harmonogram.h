2cfe4a7867bb143eaa7239f4ae470d6b231d33cf
patryktoplane <113851282+patryktoplane@users.noreply.github.com>
2026-03-27 12:52:00 +0100

    Create Harmonogram.h

---
+++ Harmonogram.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Czas.h"
+#include<iostream>
+
+using namespace std;
+
+class Harmonogram
+{
+private:
+	Czas* czas = nullptr;
+	int liczbaCzasow = 0;
+	int rozmiar = 0;
+
+public:
+	Harmonogram();
+	~Harmonogram();
+
+	void addCzas(Czas& newCzas);
+	int getLiczbaCzasow();
+
+	Czas& operator[](int x);
+
+	void printHarmonogram();
+	Czas sumaCzasow();
+
+};
