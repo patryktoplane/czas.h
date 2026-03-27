ec58fdffffd26d8a9f5e279a532fee0debe0383d
patryktoplane <113851282+patryktoplane@users.noreply.github.com>
2026-03-27 12:52:12 +0100

    Implement Harmonogram class with time management

---
+++ Harmonogram.cpp
@@ -0,0 +1,64 @@
+#include "Harmonogram.h"
+#include "Czas.h"
+#include <iostream>
+
+using namespace std;
+
+Harmonogram::Harmonogram()
+{
+	czas = nullptr;
+	liczbaCzasow = 0;
+	rozmiar = 0;
+}
+Harmonogram::~Harmonogram()
+{
+	delete[] czas;
+}
+
+void Harmonogram::addCzas(Czas& newCzas)
+{
+	if (liczbaCzasow >= rozmiar)
+	{
+		rozmiar++;
+		Czas* TablicaCzas = new Czas[rozmiar];
+		for (int i = 0; i < liczbaCzasow; i++)
+		{
+			TablicaCzas[i] = czas[i];
+		}
+		delete[] czas;
+		czas = TablicaCzas;
+	}
+	czas[liczbaCzasow] = newCzas;
+	liczbaCzasow++;
+}
+
+int Harmonogram::getLiczbaCzasow()
+{
+	return liczbaCzasow;
+}
+
+Czas& Harmonogram::operator[](int x)
+{
+	if (x >= 0 && x < liczbaCzasow)
+		return czas[x];
+}
+
+void Harmonogram::printHarmonogram()
+{
+	for (int i = 0; i < liczbaCzasow; i++)
+	{
+		czas[i].printCzas();
+	}
+}
+
+Czas Harmonogram::sumaCzasow()
+{
+	Czas wynik;
+
+	for (int i = 0; i < liczbaCzasow; i++)
+	{
+		wynik += czas[i];
+	}
+
+	return wynik;
+}
