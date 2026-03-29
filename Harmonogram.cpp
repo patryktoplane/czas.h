1436aadf5eb3044faaa75c5f72706996e3b7bfeb
patryktoplane <113851282+patryktoplane@users.noreply.github.com>
2026-03-29 20:30:44 +0200

    Update Harmonogram.cpp

--- Harmonogram.cpp
+++ Harmonogram.cpp
@@ -7,9 +7,19 @@ using namespace std;
 Harmonogram::Harmonogram()
 {
 	czas = nullptr;
-	liczbaCzasow = 0;
 	rozmiar = 0;
 }
+
+Harmonogram::Harmonogram(const Harmonogram& newHarmonogram)
+{
+	rozmiar = newHarmonogram.rozmiar;
+	czas = new Czas[rozmiar];
+	for (int i = 0; i < rozmiar; i++)
+	{
+		czas[i] = newHarmonogram.czas[i];
+	}
+}
+
 Harmonogram::~Harmonogram()
 {
 	delete[] czas;
@@ -17,36 +27,83 @@ Harmonogram::~Harmonogram()
 
 void Harmonogram::addCzas(Czas& newCzas)
 {
-	if (liczbaCzasow >= rozmiar)
+	Czas* TablicaCzas = new Czas[rozmiar+1];
+	for (int i = 0; i < rozmiar; i++)
 	{
-		rozmiar++;
-		Czas* TablicaCzas = new Czas[rozmiar];
-		for (int i = 0; i < liczbaCzasow; i++)
-		{
-			TablicaCzas[i] = czas[i];
-		}
-		delete[] czas;
-		czas = TablicaCzas;
+		TablicaCzas[i] = czas[i];
 	}
-	czas[liczbaCzasow] = newCzas;
-	liczbaCzasow++;
-}
-
-int Harmonogram::getLiczbaCzasow()
-{
-	return liczbaCzasow;
-}
-
-Czas& Harmonogram::operator[](int x)
-{
-	if (x >= 0 && x < liczbaCzasow)
-		return czas[x];
+	delete[] czas;
+	czas = TablicaCzas;
+	czas[rozmiar] = newCzas;
+	rozmiar++;
 }
 
 void Harmonogram::printHarmonogram()
 {
-	for (int i = 0; i < liczbaCzasow; i++)
+	for (int i = 0; i < rozmiar; i++)
 	{
 		czas[i].printCzas();
 	}
+}
+
+int Harmonogram::getLiczbaCzasow()
+{
+	return rozmiar;
+}
+
+Czas& Harmonogram::operator[](int x)
+{
+	if (x >= 0 && x < rozmiar)
+		return czas[x];
+}
+
+Czas Harmonogram::sumaCzasow()
+{
+	Czas wynik;
+
+	for (int i = 0; i < rozmiar; i++)
+	{
+		wynik += czas[i];
+	}
+
+	return wynik;
+}
+
+Harmonogram& Harmonogram::operator=(const Harmonogram& newHarmonogram)
+{
+	if (&newHarmonogram == this)
+		return *this;
+
+	delete[] czas;
+	rozmiar = newHarmonogram.rozmiar;
+	czas = new Czas[rozmiar];
+
+	for (int i = 0; i < rozmiar; i++)
+	{
+		czas[i] = newHarmonogram.czas[i];
+	}
+
+	return *this;
+}
+
+Harmonogram Harmonogram::kopiaDoZakresu(const Czas& zakres)
+{
+	Harmonogram nowyHarmonogram;
+	Czas suma;
+
+	for (int i = 0; i < rozmiar; i++)
+	{
+		suma += czas[i];
+
+		if (suma <= zakres)
+		{
+			nowyHarmonogram.addCzas(czas[i]);
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	return nowyHarmonogram;
 }
